9639b4233585b0601fb15f91a650e610a4dbbe42
JAifar <tetiana.kravchukqa@gmail.com>
2019-08-01 10:54:42 -0400

    11 lesson 5th part

--- src/test/java/Conditions.java
+++ src/test/java/Conditions.java
@@ -183,7 +183,7 @@ public class Conditions extends BaseUI {
         String expectedTitleStoreDepartments = "Store Departments";
         String expectedTitleNewsAndMedia = "News & Media";
         String expectedTitleCommunity = "Community";
-        List<WebElement> links = driver.findElements(By.xpath("//*[@id=\"webcenter-site-body-container\"]/footer/div[2]/div[1]/div[2]/ul"));
+        List<WebElement> links = driver.findElements(By.xpath("(//ul[@class='c-list--no-bullets'])[5]//li"));
         System.out.println(links.size());
 
         for (int i = 0; i < links.size(); i++) {
@@ -192,22 +192,41 @@ public class Conditions extends BaseUI {
             //Thread.sleep(3000);
             links.get(i).click();
             driver.get(Data.HomeURL);
-            links = driver.findElements(By.xpath("//*[@id=\"webcenter-site-body-container\"]/footer/div[2]/div[1]/div[2]/ul"));
+            links = driver.findElements(By.xpath("(//ul[@class='c-list--no-bullets'])[5]//li"));
             if (info.contains("Our Story")) {
-                String actualTitle = driver.findElement(By.xpath("//*[@id=\"about-us-link-qa\"]")).getText();
+                String actualTitle = driver.findElement(By.xpath("//a[@id='about-us-link-qa']")).getText();
+                //(//a[@href="/about"])[2]
                 Assert.assertEquals(expectedTitleOurStory, actualTitle);
                 System.out.println(actualTitle);
 
             }
             if (info.contains("Private Brands")) {
-                String actualTitle = driver.findElement(By.xpath("//li[@class='c-utility-list-xlarge__item']//a[@href=\"/health-and-wellness\"]")).getText();
+                String actualTitle = driver.findElement(By.xpath("//a[@id='own-brands-link-qa']")).getText();
                 Assert.assertEquals(expectedTitlePrivateBrands, actualTitle);
                 System.out.println(actualTitle);
 
             }
             if (info.contains("Fresh Stories")) {
-                String actualTitle = driver.findElement(By.xpath("//li[@class='c-utility-list-xlarge__item is-anonymous-xlarge -blue']//a[@href=\"/sign-up\"]")).getText();
+                String actualTitle = driver.findElement(By.xpath("//a[@id='fresh-stories-link-qa']")).getText();
                 Assert.assertEquals(expectedTitleFreshStories, actualTitle);
+                System.out.println(actualTitle);
+
+            }
+            if (info.contains("Store Departments")) {
+                String actualTitle = driver.findElement(By.xpath("//a[@id='store-departments-link-qa']")).getText();
+                Assert.assertEquals(expectedTitleStoreDepartments, actualTitle);
+                System.out.println(actualTitle);
+
+            }
+            if (info.contains("News & Media")) {
+                String actualTitle = driver.findElement(By.xpath("//a[@id='news-link-qa']")).getText();
+                Assert.assertEquals(expectedTitleNewsAndMedia, actualTitle);
+                System.out.println(actualTitle);
+
+            }
+            if (info.contains("Community")) {
+                String actualTitle = driver.findElement(By.xpath("//a[@id='community-link-qa']")).getText();
+                Assert.assertEquals(expectedTitleCommunity, actualTitle);
                 System.out.println(actualTitle);
 
             }
