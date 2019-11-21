e356159e58e3e70fce51c7930fc5eef6c91721a0
JAifar <tetiana.kravchukqa@gmail.com>
2019-11-21 17:23:29 -0500

    links check

--- src/main/java/BaseActions.java
+++ src/main/java/BaseActions.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.SecureRandom;
 import java.util.List;
@@ -70,12 +72,46 @@ public class BaseActions{ //created class for simple actions what is not connect
         }
     }
 
+    public  void verifyLink(String urlLink) {
+        //Sometimes we may face exception "java.net.MalformedURLException". Keep the code in try catch block to continue the broken link analysis
+        try {
+            //Use URL Class - Create object of the URL Class and pass the urlLink as parameter
+            URL link = new URL(urlLink);
+            // Create a connection using URL object (i.e., link)
+            HttpURLConnection httpConn =(HttpURLConnection)link.openConnection();
+            //Set the timeout for 2 seconds
+            httpConn.setConnectTimeout(2000);
+            //connect using connect method
+            httpConn.connect();
+            //use getResponseCode() to get the response code.
+            if(httpConn.getResponseCode()== 200) {
+                System.out.println(urlLink+" - "+httpConn.getResponseMessage());
+            }
+            if(httpConn.getResponseCode()== 404) {
+                System.out.println(urlLink+" - "+httpConn.getResponseMessage());
+            }
+            if(httpConn.getResponseCode()== 400) {
+                System.out.println(urlLink+" - "+httpConn.getResponseMessage()); }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(httpConn.getResponseCode()== 500) {
+            System.out.println(urlLink+" - "+ httpConn.getResponseMessage()); }
 
-    
+//getResponseCode method returns = IOException - if an error occurred connecting to the server.
+        catch (Exception e){
+                //e.printStackTrace();
+
+                }}
 
 
 
-    public void ajaxClick(By by, int index){
+
+
+
+public void ajaxClick(By by, int index){
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
         wait.until(ExpectedConditions.elementToBeClickable(by));
         ajaxClick(driver.findElements(by).get(index));
